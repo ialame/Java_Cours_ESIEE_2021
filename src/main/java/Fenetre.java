ea4802277b9c1b093693d8a940096908e0d3ade9
Ibrahim ALAME <ibrahim.alame@gmail.com>
2021-06-11 09:01:20 +0200

    deuxième commit

--- src/main/java/Fenetre.java
+++ src/main/java/Fenetre.java
@@ -5,9 +5,9 @@ public class Fenetre extends JFrame {
     public Boule boule;
     public Fenetre(){
         setTitle("Ma fenêtre Java");
-        setSize(400,300);
+        setSize(1000,1000);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        boule = new Boule(20,20,75,75);
+        boule = new Boule();
         setContentPane(boule);
         //Container panneau = getContentPane();
         setVisible(true);
@@ -16,42 +16,16 @@ public class Fenetre extends JFrame {
     }
     public static void main(String[] str){
         Fenetre f =new Fenetre();
-        for(int i=0;i<100;i++) {
-            f.boule.setX(100);
-            f.boule.setX(200);
+        for(int i=0;i<200;i++) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            f.boule.setPosX(2*i);
+            f.boule.setPosY(2*i);
+            f.boule.repaint();
 
-        }
-    }
-    class Boule extends JPanel{
-        int x=0,y=0,w=100,h=100;
-        public Boule(int x, int y, int w, int h) {
-            this.x = x;
-            this.y = y;
-            this.w = w;
-            this.h = h;
-        }
-
-        public void setX(int x) {
-            this.x = x;
-        }
-
-        public void setY(int y) {
-            this.y = y;
-        }
-
-        public void setW(int w) {
-            this.w = w;
-        }
-
-        public void setH(int h) {
-            this.h = h;
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            System.out.println("Je suis exécutée !");
-            g.fillOval(x,y,w,h);
         }
     }
 }
---
+++ src/main/java/ProblemeDeTir.java
@@ -0,0 +1,70 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class ProblemeDeTir extends JFrame {
+
+    public static void main(String[] args) {
+        new ProblemeDeTir();
+    }
+
+    private Panneau pan = new Panneau();
+
+    public ProblemeDeTir() {
+        this.setTitle("Animation");
+        this.setSize(1000, 1000);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setContentPane(pan);
+        this.setVisible(true);
+
+        go();
+    }
+
+    private void go() {
+        // Les coordonnées de départ de notre rond
+        //int x = pan.getPosX(), y = pan.getPosY();
+        // Le booléen pour savoir si l'on recule ou non sur l'axe x
+        boolean backX = false;
+        // Le booléen pour savoir si l'on recule ou non sur l'axe y
+        boolean backY = false;
+
+        // Dans cet exemple, j'utilise une boucle while
+        // Vous verrez qu'elle fonctionne très bien
+        double x=100;
+        double y=100;
+        double alpha=Math.PI/6;
+        double V = 200;
+        double Vx=V*Math.cos(alpha),Vy=-V*Math.sin(alpha);
+        double dVx,dVy;
+        double dx,dy;
+        double g=5,Ax,Ay;
+        double dt=0.01;
+        while (true) {
+            // Si la coordonnée x est inférieure à 1, on avance
+
+            Ax=0;Ay=g;
+            dVx=Ax;dVy=Ay;
+            Vx=Vx+dVx; Vy=Vy+dVy;
+            if (y>1000-50) Vy=-Vy;
+            if (x>1000-50) Vx=-Vx;
+            dx = Vx*dt;
+            dy = Vy*dt;
+
+            x=x+dx;
+            y=y+dy;
+            //System.out.println(x+" "+y);
+            pan.setPosX((int)x);
+            pan.setPosY((int)y);
+
+
+            // On redessine notre Panneau
+            pan.repaint();
+            // Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
